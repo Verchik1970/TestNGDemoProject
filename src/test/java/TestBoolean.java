26c40d171210e5f9592d25561c4d89ce3cac14a7
Verchik1970 <Verchik1970@gmail.com>
2023-07-30 15:40:09 +0300

    allure + dataProvider + allureReport

--- src/test/java/TestBoolean.java
+++ src/test/java/TestBoolean.java
@@ -1,29 +1,38 @@
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.time.LocalDate;
 import static org.testng.Assert.assertEquals;
 
-public class TestBoolean {
-    double a = 4.0;
-    double b = 2.0;
 
+public class TestBoolean {
+    @DataProvider(name = "numbers")
+    public Object[][] generateNumbers() {
+        return new Object[][]{
+                {2.0, 2.0},
+                {2.0, 3.0},
+                {2.0, 4.0}
+
+        };
+    }
+
+
+
+    @Test(dataProvider = "numbers")
+    public void testChekN(double a, double b) {
+        CheckTheNumber Ch = new CheckTheNumber();
+        double result = Ch.Stepen(a, b);
+        Assert.assertEquals(result, 8);
+    }
 
     @Test
     public void testDate() {
         LocalDate date = LocalDate.now();
-        Assert.assertEquals(date.getYear(), 2023);
-        Assert.assertEquals(date.getMonthValue(), 7);
-        Assert.assertEquals(date.getDayOfMonth(), 30);
+        assertEquals(date.getYear(), 2023);
+        assertEquals(date.getMonthValue(), 7);
+        assertEquals(date.getDayOfMonth(), 30);
     }
 
-
-    @Test
-    public void testChekN() {
-        CheckTheNumber Ch = new CheckTheNumber();
-        double result = Ch.Stepen(a, b);
-        assertEquals(25.0, result);
-
-
-    }
 
 }
